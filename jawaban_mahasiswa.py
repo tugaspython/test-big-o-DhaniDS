a863219539d3c85e7c3d7aa780c3f2676863df7f
DhaniDS <dhanidwinawans12@gmail.com>
2026-06-13 19:12:31 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -1,1 +1,10 @@
-
+# File: jawaban_mahasiswa.py (Solusi Profesional)
+def cari_pasangan(arr, target):
+    # Menggunakan Set (Kompleksitas O(N))
+    angka_dilihat = set()
+    for angka in arr:
+        pasangan_dibutuhkan = target - angka
+        if pasangan_dibutuhkan in angka_dilihat:
+            return True
+        angka_dilihat.add(angka)
+    return False
